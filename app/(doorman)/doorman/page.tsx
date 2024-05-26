bb4ed8d62ee96891a2776dcfd746683f8a1f737d
galileo-dev <fionn2200@gmail.com>
2024-05-26 12:38:42 +0100

    door man

--- app/(doorman)/doorman/page.tsx
+++ app/(doorman)/doorman/page.tsx
@@ -1,5 +1,14 @@
+"use client";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
+import { useDisconnect } from "wagmi";
 
 export default function Doorman() {
+  const { disconnect } = useDisconnect();
+
+  useEffect(() => {
+    disconnect();
+  }, []);
+
   redirect("/doorman/wallets");
 }
