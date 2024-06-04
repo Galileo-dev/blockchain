25bf5eec10a0dbc0abd40b9341b8387b0ad806b3
galileo-dev <fionn2200@gmail.com>
2024-06-04 13:53:27 +0100

    safer address set

--- config/contracts.ts
+++ config/contracts.ts
@@ -1,7 +1,9 @@
-import { Address, erc20Abi } from "viem";
+import { erc20Abi } from "viem";
 
-const address = (process.env.NEXT_PUBLIC_TICKET_TOKEN_ADDRESS ||
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3") as Address;
+const address = //is development
+  process.env.NODE_ENV === "development"
+    ? "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    : process.env.NEXT_PUBLIC_TICKET_TOKEN_ADDRESS!;
 
 const buyTicketAbi = {
   constant: false,
