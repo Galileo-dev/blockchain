f90d8677eb000228725cb3584d8d69062ba54274
galileo-dev <fionn2200@gmail.com>
2024-06-04 14:25:41 +0100

    fix address type

--- config/contracts.ts
+++ config/contracts.ts
@@ -3,7 +3,7 @@ import { erc20Abi } from "viem";
 const address =
   process.env.NODE_ENV === "development"
     ? "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-    : process.env.NEXT_PUBLIC_TICKET_TOKEN_ADDRESS!;
+    : (process.env.NEXT_PUBLIC_TICKET_TOKEN_ADDRESS as `0x${string}`);
 
 const buyTicketAbi = {
   constant: false,
