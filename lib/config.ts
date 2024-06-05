22ec8995829977a45409e6f5ed64f275ea4c4050
galileo-dev <fionn2200@gmail.com>
2024-06-05 13:59:58 +0100

    fix down sepolia.org

--- lib/config.ts
+++ lib/config.ts
@@ -2,12 +2,23 @@ import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
 import { localhost, sepolia } from "wagmi/chains";
 
-const isLocalhost = process.env.NODE_ENV === "development";
+const isLocalhost =
+  process.env.NODE_ENV === "development" &&
+  typeof process.env.NEXT_PUBLIC_TICKET_TOKEN_ADDRESS === undefined;
+
+const sepoliaBetterRpc = {
+  ...sepolia,
+  rpcUrls: {
+    default: {
+      http: ["https://sepolia.infura.io/v3/e64e0f234c6c45a3b55011391da6fe61"],
+    },
+  },
+};
 
 export const config = createConfig(
   getDefaultConfig({
     ssr: true,
-    chains: isLocalhost ? [localhost] : [sepolia],
+    chains: isLocalhost ? [localhost] : [sepoliaBetterRpc],
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
     appName: "Orb",
     transports: isLocalhost
@@ -15,7 +26,7 @@ export const config = createConfig(
           [localhost.id]: http(),
         }
       : {
-          [sepolia.id]: http(),
+          [sepoliaBetterRpc.id]: http(),
         },
   }),
 );
